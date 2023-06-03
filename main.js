65c5c565687f771eb7f3e01d480d8c4060ba28b6
daisuke-watanabe <pitagora.3.swich@gmail.com>
2023-06-03 16:30:40 +0900

    jsファイルのベースを追記

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <p>テスト</p>
-        <p id="count" class="count">60</p>
-        <div id="wrap" class="wrap">
-            <span id="typed" class="typed"></span><span id="untyped">Hello World</span>
-        </div>
-        <button id="start">スタート</button>
-    </body>
-</html>
+const createText = () => {};
+
+const keyPress = e => {};
+
+const rankCheck = score => {};
+
+const timer = () => {};
